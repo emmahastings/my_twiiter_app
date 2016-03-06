3e92aa5f6de4e34970789aa401d16e26de050504
emmahastings <emmakhastings@gmail.com>
2016-03-06 15:14:33 +0000

    First refactor as part of phase 2

---
+++ model/src/main/java/SearchForm.java
@@ -0,0 +1,23 @@
+/**
+ * Created by emmakhastings on 02/03/2016.
+ * @author emmakhastings
+ */
+public class SearchForm {
+
+    private String query;
+
+    public SearchForm() {
+    }
+
+    public SearchForm(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
