74e9aef5c3823b2bb92909446abcacd94ac34133
emmahastings <emmakhastings@gmail.com>
2016-04-02 13:35:04 +0100

    Formatting

--- sd-interface/src/main/java/dashboard/controller/HomeController.java
+++ sd-interface/src/main/java/dashboard/controller/HomeController.java
@@ -1,30 +1,17 @@
 
 package dashboard.controller;
 
-
-import model.SearchForm;
-import model.TweetDetails;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.twitter.api.SearchParameters;
-import org.springframework.social.twitter.api.SearchResults;
-import org.springframework.social.twitter.api.Tweet;
-import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by emmakhastings on 02/03/2016.
  *
  * @author emmakhastings
- *
- * Home contoller returning the home view with a list of various options
+ *         <p>
+ *         Home contoller returning the home view with a list of various options
  */
 
 @Controller
@@ -35,4 +22,3 @@ public class HomeController {
         return "home";
     }
 }
-
