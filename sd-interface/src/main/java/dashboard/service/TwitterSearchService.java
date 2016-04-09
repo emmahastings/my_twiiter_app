f4857989fc67f84090ba032cbd9bab04b2b269f2
emmahastings <emmakhastings@gmail.com>
2016-04-09 18:00:37 +0100

    New service to carry out twitter search

---
+++ sd-interface/src/main/java/dashboard/service/TwitterSearchService.java
@@ -0,0 +1,46 @@
+package dashboard.service;
+
+import model.TweetDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchParameters;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by emmakhastings on 09/04/2016.
+ *
+ * @author emmakhastings
+ *         <p>
+ *         Service to perform a twitter search
+ */
+public class TwitterSearchService {
+
+    private Twitter twitter;
+
+    @Autowired
+    public TwitterSearchService(Twitter twitter) {
+        this.twitter = twitter;
+    }
+
+    public List<TweetDetails> search(String query) {
+        SearchParameters params = new SearchParameters(query);
+        params.lang("en");
+
+        SearchResults results = twitter.searchOperations().search(params);
+        List<TweetDetails> tweets = new ArrayList<>();
+
+        for (Tweet tweet : results.getTweets()) {
+            TweetDetails tweetDetails = new TweetDetails();
+            tweetDetails.setTweet(tweet.getText());
+            tweetDetails.setUserName(tweet.getFromUser());
+            tweetDetails.setUserUrl(tweet.getUser().getUrl());
+            tweetDetails.setUserLocation(tweet.getUser().getLocation());
+            tweets.add(tweetDetails);
+        }
+        return tweets;
+    }
+}
